99d663e5db36d608bcd4c1f21bd87fdd63501905
Peter Wong <peterwcm@hotmail.com>
2017-01-06 23:53:21 +1000

    remove font awesome files and update grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = grunt => {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'assets/js/app.js',
+                dest: 'build/js/app.min.js'
+            }
+        },
         sass: {
             dist: {
                 options: {
@@ -8,17 +17,22 @@ module.exports = grunt => {
                     style: 'compressed'
                 },
                 files: {
-                    'compiled/css/app-min.css' : 'assets/sass/app.scss'
+                    'build/css/app.min.css' : 'assets/sass/app.scss'
                 }
             }
         },
         watch: {
+            js: {
+                files: 'assets/js/app.js',
+                tasks: ['uglify']
+            },
             css: {
                 files: 'assets/sass/**/*.scss',
                 tasks: ['sass']
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['watch']);
