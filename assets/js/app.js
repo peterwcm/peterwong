db95bc9a23701d0951473581b342632baa4f5495
Peter Wong <peterwcm@hotmail.com>
2017-01-09 23:03:26 +1000

    fixing loading animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@ const menu = {
 
 const section = {
    init: () => {
-      const LOADING_TIME = 1000;
+      const LOADING_TIME = 1500;
       // Update section min height.
       section.updateHeight(windowHeight);
       // Create a fake loading effect by setting a 1s time out.
