c292ca511e44328582d0542c5a7044d1aac0a510
Peter Wong <peterwcm@hotmail.com>
2017-01-16 20:21:22 +1000

    add grunt build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,4 +89,5 @@ module.exports = grunt => {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['concat', 'babel', 'uglify', 'sass', 'postcss']);
 };
