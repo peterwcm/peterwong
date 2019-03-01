fb57892d66788136b8c83ddfee534b1ea8900eb6
Peter Wong <peterwong.brisbane@gmail.com>
2019-03-01 22:27:58 +0800

    Fixed index page css/js loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,160 +1,172 @@
 class Menu {
-    constructor() {
-        this._stickyOffest = 200;
-        this._stickyClass = 'is-sticky';
-    }
+  constructor() {
+    this._stickyOffest = 200;
+    this._stickyClass = 'is-sticky';
+  }
 
-    init() {
-        const $window = $(window);
-        const $menu = $('.magellan-container');
+  init() {
+    const $window = $(window);
+    const $menu = $('.magellan-container');
 
-        $window.scroll(() => {
-            $window.scrollTop() > this._stickyOffest
-                ? $menu.addClass(this._stickyClass)
-                : $menu.removeClass(this._stickyClass);
-        });
-    }
+    $window.scroll(() => {
+      $window.scrollTop() > this._stickyOffest
+        ? $menu.addClass(this._stickyClass)
+        : $menu.removeClass(this._stickyClass);
+    });
+  }
 }
 
 class Section {
-    constructor(loadingTime) {
-        this._loadingTime = loadingTime;
-        this._windowHeight = $(window).height();
-        this._scrollOffsets = [];
-    }
+  constructor(loadingTime) {
+    this._loadingTime = loadingTime;
+    this._windowHeight = $(window).height();
+    this._scrollOffsets = [];
+  }
 
-    init() {
-        // Create a fake loading effect.
-        this._loadPage(() => {
-            // Update section min height and scroll offsets.
-            this._updateHeight();
-            this.refreshScroll();
-            // Initial window resize handler to update min height.
-            this._initResizeHandler();
-            // Initial scroll handler.
-            this._initScrollHandler();
-        });
-    }
+  init() {
+    // Create a fake loading effect.
+    this._loadPage(() => {
+      // Update section min height and scroll offsets.
+      this._updateHeight();
+      this.refreshScroll();
+      // Initial window resize handler to update min height.
+      this._initResizeHandler();
+      // Initial scroll handler.
+      this._initScrollHandler();
+    });
+  }
 
-    refreshScroll() {
-        this._updateScrollOffsets();
-        this._updateScroll();
-    }
+  refreshScroll() {
+    this._updateScrollOffsets();
+    this._updateScroll();
+  }
 
-    _loadPage(onFinishedLoading) {
-        setTimeout(() => {
-            $('body').addClass('section-loaded');
-            onFinishedLoading();
-        }, this._loadingTime);
-    }
+  _loadPage(onFinishedLoading) {
+    setTimeout(() => {
+      $('body').addClass('section-loaded');
+      onFinishedLoading();
+    }, this._loadingTime);
+  }
 
-    _updateScrollOffsets() {
-        this._scrollOffsets = [];
-        $('.block--content').each((i, elem) => {
-            this._scrollOffsets.push({
-                $elem: $(elem).prev('.block--title'),
-                start: $(elem).offset().top,
-                end: $(elem).offset().top + $(elem).outerHeight() - this._windowHeight
-            });
-        });
-    }
+  _updateScrollOffsets() {
+    this._scrollOffsets = [];
+    $('.block--content').each((i, elem) => {
+      this._scrollOffsets.push({
+        $elem: $(elem).prev('.block--title'),
+        start: $(elem).offset().top,
+        end: $(elem).offset().top + $(elem).outerHeight() - this._windowHeight
+      });
+    });
+  }
 
-    _initScrollHandler() {
-        $(window).scroll(() => {
-            this._updateScroll();
-        });
-    }
+  _initScrollHandler() {
+    $(window).scroll(() => {
+      this._updateScroll();
+    });
+  }
 
-    _initResizeHandler() {
-        const $window = $(window);
+  _initResizeHandler() {
+    const $window = $(window);
 
-        $window.resize(() => {
-            const currentHeight = $window.height();
-            if (currentHeight && currentHeight !== this._windowHeight) {
-                this._windowHeight = currentHeight;
-                this._updateHeight();
-            }
-            this.refreshScroll();
-        });
-    }
+    $window.resize(() => {
+      const currentHeight = $window.height();
+      if (currentHeight && currentHeight !== this._windowHeight) {
+        this._windowHeight = currentHeight;
+        this._updateHeight();
+      }
+      this.refreshScroll();
+    });
+  }
 
-    _updateScroll() {
-        const $window = $(window);
+  _updateScroll() {
+    const $window = $(window);
 
-        this._scrollOffsets.forEach(offset => {
-            if ($window.scrollTop() >= offset.start
-                && $window.scrollTop() < offset.end) {
-                offset.$elem
-                    .removeClass('scroll-start scroll-end')
-                    .addClass('scroll-start');
-            } else if ($window.scrollTop() >= offset.end) {
-                offset.$elem
-                    .removeClass('scroll-start scroll-end')
-                    .addClass('scroll-end');
-            } else {
-                offset.$elem.removeClass('scroll-start scroll-end');
-            }
-        });
-    }
+    this._scrollOffsets.forEach(offset => {
+      if ($window.scrollTop() >= offset.start
+        && $window.scrollTop() < offset.end) {
+        offset.$elem
+          .removeClass('scroll-start scroll-end')
+          .addClass('scroll-start');
+      } else if ($window.scrollTop() >= offset.end) {
+        offset.$elem
+          .removeClass('scroll-start scroll-end')
+          .addClass('scroll-end');
+      } else {
+        offset.$elem.removeClass('scroll-start scroll-end');
+      }
+    });
+  }
 
-    _updateHeight() {
-        $('.section--full-page, .block--full-page')
-            .css('min-height', this._windowHeight);
-    }
+  _updateHeight() {
+    $('.section--full-page, .block--full-page')
+      .css('min-height', this._windowHeight);
+  }
 }
 
 class Work {
-    constructor(section) {
-        this._menuHeight = 60;
-        this._section = section;
-        this._delay = 500;
-    }
+  constructor(section) {
+    this._menuHeight = 60;
+    this._section = section;
+    this._delay = 500;
+  }
 
-    init() {
-        $('.work__more').click(e => {
-            const $elem = $(e.currentTarget);
-            const id = $elem.data('id');
-            const $details = $(`.work__details[data-id='${id}']`);
+  init() {
+    $('.work__more').click(e => {
+      const $elem = $(e.currentTarget);
+      const id = $elem.data('id');
+      const $details = $(`.work__details[data-id='${id}']`);
 
-            $details.slideDown();
-            setTimeout(() => {
-                this._section.refreshScroll();
-                $('html, body').animate({
-                    scrollTop: $details.offset().top - this._menuHeight
-                });
-            }, this._delay);
+      $details.slideDown();
+      setTimeout(() => {
+        this._section.refreshScroll();
+        $('html, body').animate({
+          scrollTop: $details.offset().top - this._menuHeight
         });
+      }, this._delay);
+    });
 
 
-        $('.work__less').click(e => {
-            const $elem = $(e.currentTarget);
-            const id = $elem.data('id');
-            const $details = $(`.work__details[data-id='${id}']`);
+    $('.work__less').click(e => {
+      const $elem = $(e.currentTarget);
+      const id = $elem.data('id');
+      const $details = $(`.work__details[data-id='${id}']`);
 
-            $details.slideUp();
-            setTimeout(() => {
-                this._section.refreshScroll();
-            }, this._delay);
-        });
-    }
+      $details.slideUp();
+      setTimeout(() => {
+        this._section.refreshScroll();
+      }, this._delay);
+    });
+  }
+}
+
+class Prefill {
+  constructor() {
+    this._currentYear = new Date().getFullYear();
+  }
+
+  init() {
+    $('.js-prefill-year').text(this._currentYear);
+  }
 }
 
 $(() => {
-    $(document).foundation({
-        'magellan-expedition': {
-            active_class: 'is-active',
-            destination_threshold: 0
-        }
-    });
+  $(document).foundation({
+    'magellan-expedition': {
+      active_class: 'is-active',
+      destination_threshold: 0
+    }
+  });
 
-    const isLocal = location.protocol === 'file:';
-    // if (isLocal) $('.work__details').show();
-    const loadingTime = isLocal ? 1000 : 3000;
+  const isLocal = location.protocol === 'file:';
+  // if (isLocal) $('.work__details').show();
+  const loadingTime = isLocal ? 1000 : 3000;
 
-    const menu = new Menu();
-    menu.init();
-    const section = new Section(loadingTime);
-    section.init();
-    const work = new Work(section);
-    work.init();
+  const prefill = new Prefill();
+  prefill.init();
+  const menu = new Menu();
+  menu.init();
+  const section = new Section(loadingTime);
+  section.init();
+  const work = new Work(section);
+  work.init();
 });
