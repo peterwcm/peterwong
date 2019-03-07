12884fa33dc1673fd374856d1119786af7c3747e
Peter Wong <peterwong.brisbane@gmail.com>
2019-03-08 00:23:38 +0800

    Basic hashtag generations

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -14,9 +14,21 @@ class Hashtags {
     ];
   }
 
+  _getFilteredHashtags(hashtagType) {
+    return $.grep(hashtags, (obj) => {
+      return obj.types.indexOf(type[hashtagType]) !== -1;
+    });
+  }
+
   init() {
     $('.js-hashtags-form').submit(() => {
-      $('.js-hashtags').text('test');
+      let filteredHashtags = this._getFilteredHashtags('any');
+
+      $('input[type="checkbox"]:checked').each((index, elem) => {
+        $.merge(filteredHashtags, this._getFilteredHashtags($(elem).val()));
+      });
+
+      $('.js-hashtags').text(filteredHashtags.map(elem => `#${elem.name}`).join(' '));
       return false;
     });
   }
@@ -30,18 +42,42 @@ $(() => {
 const type = {
   any: 0,
   travel: 1,
+  landscape: 2,
+  portrait: 3,
+  urban: 4,
+  nature: 5,
+  food: 6,
+  couples: 7
 };
 
 const hashtags = [
   {'name': 'bestoftheday', 'size': 'large', 'types': [type.any]},
-  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+  {'name': 'follow', 'size': 'large', 'types': [type.any]},
+  {'name': 'followme', 'size': 'large', 'types': [type.any]},
+  {'name': 'igers', 'size': 'large', 'types': [type.any]},
   {'name': 'instadaily', 'size': 'large', 'types': [type.any]},
-  {'name': 'instatravel', 'size': 'large', 'types': [type.travel]},
+  {'name': 'instagood', 'size': 'large', 'types': [type.any]},
+  {'name': 'instagram', 'size': 'large', 'types': [type.any]},
+  {'name': 'like4like', 'size': 'large', 'types': [type.any]},
+  {'name': 'likeforlike', 'size': 'large', 'types': [type.any]},
+  {'name': 'photo', 'size': 'large', 'types': [type.any]},
+  {'name': 'photography', 'size': 'large', 'types': [type.any]},
   {'name': 'photooftheday', 'size': 'large', 'types': [type.any]},
   {'name': 'picoftheday', 'size': 'large', 'types': [type.any]},
+
+
+  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+  {'name': 'instatravel', 'size': 'large', 'types': [type.travel]},
   {'name': 'travel', 'size': 'large', 'types': [type.travel]},
   {'name': 'traveling', 'size': 'large', 'types': [type.travel]},
   {'name': 'wanderlust', 'size': 'large', 'types': [type.travel]},
+
+  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+  {'name': 'explorer', 'size': 'large', 'types': [type.travel]},
+
   {'name': '', 'size': '', 'types': []},
   {'name': '', 'size': '', 'types': []}
 
