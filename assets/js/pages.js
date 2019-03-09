38e3e60df1ef838736ebfdba434bf8c2f839cca7
Peter Wong <peterwong.brisbane@gmail.com>
2019-03-09 11:31:06 +0800

    Complete hashtag generation algorithm

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -8,27 +8,132 @@ class Hashtags {
       // > 500k, pick 6 (20%).
       'large': [],
     };
-
-    this._hashtags.small = [
-
-    ];
+    this._maxHashtagsNum = 30;
+    this._sHashtagsNum = 9;
+    this._mHashtagsNum = 15;
+    this._lHashtagsNum = 6;
   }
 
+  /**
+   * Get certain hashtags type from the global collection.
+   *
+   * @param {string} hashtagType
+   *   The hashtag type.
+   *
+   * @returns {array}
+   *   The list of hashtag objects array.
+   * @private
+   */
   _getFilteredHashtags(hashtagType) {
-    return $.grep(hashtags, (obj) => {
-      return obj.types.indexOf(type[hashtagType]) !== -1;
-    });
+    return $.grep(hashtags, obj => obj.types.indexOf(type[hashtagType]) !== -1);
+  }
+
+  /**
+   * Shuffle the list items.
+   *
+   * @param {array} items
+   *   The list.
+   *
+   * @returns {*}
+   *   List of shuffled items.
+   * @private
+   */
+  _shuffle(items) {
+    let currentIndex = items.length;
+    let temporaryValue;
+    let randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = items[currentIndex];
+      items[currentIndex] = items[randomIndex];
+      items[randomIndex] = temporaryValue;
+    }
+
+    return items;
+  }
+
+  /**
+   * Check if the hashtags collection is completely empty.
+   *
+   * @returns {boolean}
+   *   True if the collection is completely empty, otherwise, false.
+   * @private
+   */
+  _isHashtagsEmpty() {
+    for (let list of Object.values(this._hashtags)) {
+      if (list.length) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Trim hashtags, remove duplicates and make sure it's less than or equal the maximum number of hashtags.
+   *
+   * @param {array} hashtags
+   *   The list of hashtags.
+   *
+   * @returns {*}
+   *   List of clean hashtags.
+   * @private
+   */
+  _clean(hashtags) {
+    const cleanHashtags = hashtags.map(hashtag => hashtag.trim());
+    return cleanHashtags.sort().filter((e, i, a) => i === a.indexOf(e)).slice(0, this._maxHashtagsNum);
   }
 
   init() {
     $('.js-hashtags-form').submit(() => {
-      let filteredHashtags = this._getFilteredHashtags('any');
 
+      // Combine all hashtag objects based on user selections.
+      let filteredHashtags = this._getFilteredHashtags('any');
       $('input[type="checkbox"]:checked').each((index, elem) => {
         $.merge(filteredHashtags, this._getFilteredHashtags($(elem).val()));
       });
 
-      $('.js-hashtags').text(filteredHashtags.map(elem => `#${elem.name}`).join(' '));
+      // Randomise hashtag objects.
+      filteredHashtags = this._shuffle(filteredHashtags);
+
+      // Fill large, medium, small hashtags.
+      filteredHashtags.forEach(elem => {
+        switch(elem.size) {
+          case 'small':
+            if (this._hashtags.small.length < this._sHashtagsNum) {
+              this._hashtags.small.push(elem.name);
+            }
+            break;
+          case 'medium':
+            if (this._hashtags.medium.length < this._mHashtagsNum) {
+              this._hashtags.medium.push(elem.name);
+            }
+            break;
+          case 'large':
+            if (this._hashtags.large.length < this._lHashtagsNum) {
+              this._hashtags.large.push(elem.name);
+            }
+            break;
+          default:
+            break;
+        }
+      });
+
+      // Start with custom hashtags and add from large, medium, small while not reaching 30.
+      let hashtagsOutput = this._clean($('#custom_hashtags').val().split('\n'));
+      while (hashtagsOutput.length < this._maxHashtagsNum && !this._isHashtagsEmpty()) {
+        for (let list of Object.values(this._hashtags)) {
+          if (list.length) {
+            hashtagsOutput.push(list.shift());
+          }
+        }
+      }
+
+      // Output hashtag results.
+      $('.js-hashtags').text(this._clean(hashtagsOutput).map(text => `#${text}`).join(' '));
       return false;
     });
   }
@@ -104,4 +209,8 @@ const hashtags = [
   // ---------- Food (M) ----------
   // ---------- Food (S) ----------
   {'name': 'tastytasty', 'size': 'small', 'types': [type.food]}
+
+  // ---------- Chinese (L) ----------
+  // ---------- Chinese (M) ----------
+  // ---------- Chinese (S) ----------
 ];
