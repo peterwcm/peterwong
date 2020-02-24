4824931185eb50e2522fb19f9d84918c877b3a2c
Peter Wong <peterwong.brisbane@gmail.com>
2020-02-24 18:32:11 +0800

    invoice updates, better privacy protection

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -2,23 +2,80 @@ class Invoice {
   constructor() { }
 
   init() {
-    this._initInputEvents();
+    this._syncUrlParams();
+    this._initInputs();
+    this.sync();
   }
 
-  _initInputEvents() {
-    $(document).dblclick(() => {
+  _syncUrlParams() {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    for (let param of urlParams) {
+
+      // Toggle edit mode.
+      if (param[0] === 'is-edit') {
+        $('body').toggleClass('is-edit', param[1] === 'true');
+        continue;
+      }
+
+      const $field = $(`.js-${param[0]}`);
+
+      if (!$field.length) {
+        continue;
+      }
+
+      if ($field.is('input')) {
+        $field.val(param[1]);
+      }
+      else {
+        $field.text(param[1]);
+      }
+    }
+  }
+
+  _initInputs() {
+    // Setup default input values.
+    $('.js-invoice-date').val(new Date().toISOString().substr(0, 10));
+
+    // Setup edit toggle event.
+    $(document).dblclick(e => {
+      const $target = $(e.target);
+
+      if ($target.hasClass('js-input')) {
+        return;
+      }
+
       const $body = $('body');
 
       $body.toggleClass('is-edit');
 
       if (!$body.hasClass('is-edit')) {
-        this._syncLabel();
-        this.updatePrice();
-
-        // Update document title.
-        document.title = "Peter's Invoice " + $('.js-invoice-num').val().replace('#', '');
+        this.sync();
       }
     });
+  }
+
+  sync() {
+    this._syncEditableLabels();
+    this._syncPriceLabels();
+    this._syncInvoiceLabels();
+  }
+
+  _syncInvoiceLabels() {
+    const $invoiceDate = $('.js-invoice-date');
+    const invoiceDateVal = $invoiceDate.val();
+    const invoiceNumber = invoiceDateVal.replace(/-/g, '');
+
+    // Update invoice date label.
+    $invoiceDate.next('.js-label').text(
+      new Date(invoiceDateVal).toLocaleString('en-AU',{month:'long', year:'numeric', day:'numeric'})
+    );
+
+    // Update invoice number.
+    $('.js-invoice-number').text(`#${invoiceNumber}`);
+
+    // Update document title.
+    document.title = `Peter's Invoice ${invoiceNumber}`;
   }
 
   _formatPrice(price) {
@@ -26,10 +83,10 @@ class Invoice {
       return '$0.00';
     }
 
-    return '$' + price.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    return `$${Number(price).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`;
   }
 
-  _syncLabel() {
+  _syncEditableLabels() {
     $('.js-editable').each((i, elem) => {
       const $elem = $(elem);
       const inputVal = $elem.find('.js-input').val();
@@ -38,13 +95,15 @@ class Invoice {
     });
   }
 
-  updatePrice() {
+  _syncPriceLabels() {
+    const $rate = $('.js-rate');
     const hours = $('.js-hours').val();
-    const rate = $('.js-rate').val();
+    const rate = $rate.val();
     const subtotal = hours * rate;
     const gst = subtotal * 0.1;
     const total = subtotal + gst;
 
+    $rate.next('.js-label').text(this._formatPrice(rate));
     $('.js-subtotal').text(this._formatPrice(subtotal));
     $('.js-gst').text(this._formatPrice(gst));
     $('.js-total').text(this._formatPrice(total));
